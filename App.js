1a1f15e8e1120e134660b6f5a069b6802c91b868
Michael Lively <michaellively00@gmail.com>
2022-12-12 19:55:25 -0600

    functioning :)

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import type {Node} from 'react';
+// import {file} from 'url';
 import {
   TouchableHighlight,
   StatusBar,
@@ -7,17 +8,44 @@ import {
   Text,
   View,
   Alert,
+  Image,
+  useState,
 } from 'react-native';
 import {BleManager, Device} from 'react-native-ble-plx';
+import {Dialog} from 'react-native-dialog';
 import {Buffer} from 'buffer';
-// import { fileInitGPX } from './gpxHandler';
 import * as RNFS from 'react-native-fs';
-import {stravaInit} from './stravaHandler';
+// import {stravaAuth} from './stravaHandler';
+import {authorize} from 'react-native-app-auth';
+import DocumentPicker from 'react-native-document-picker';
 
 const App: () => Node = () => {
+  const [visible, setVisible] = useState(false);
+
   const RIDER_DEVICE_NAME = 'STATCYCLE';
+  const CLOSE_DIR_UUID = 'c3b5d7db-997b-4207-87fb-84ac539d9fc2';
+  const OPEN_DIR_UUID = '638637bd-5780-4140-8593-ad68ed54eec1';
+  const LIST_DIR_UUID = '6094f1ba-8cf0-44bc-9b67-4ba8b30c42f3';
+  const OPEN_FILE_UUID = '7d8d6c8b-dff7-4e4c-a67b-bcac8a945d5b';
+  const READ_FILE_UUID = '4f204e4b-2ec6-427e-85b5-22430fc9626b';
+
+  const dateObj = new Date();
+  const currDay = dateObj.getDate();
+  const currMonth = dateObj.getMonth() + 1;
+  const currYear = dateObj.getFullYear();
+  // const GPXfilename = `StatCycle_GPX_${currYear}_${currMonth}_${currDay}.gpx`;
+  const GPXfilename = `StatCycle_GPX_${currYear}_${currMonth}_${currDay}_BRANDON.gpx`;
+  const XML_HEADER =
+    '<?xml version="1.0" encoding="UTF-8"?><gpx creator="StatCycle Companion GPX Generator"><trk><name>StatCycle Ride</name><type>1</type><trkseg>';
+
   const _BleManager = new BleManager();
   let driver = new Device();
+  let gpsService;
+  let pairingStatus = false;
+
+  // function checkPairing() {
+  //   return 1;
+  // }
 
   async function pairStatCycle() {
     console.log('Scan Beginning');
@@ -27,7 +55,7 @@ const App: () => Node = () => {
         allowDuplicates: false,
       },
       async (error, device) => {
-        console.log(device.name);
+        // console.log(device.name);
         if (error) {
           console.log(error);
           Alert.alert('BLE ERROR. Try again.');
@@ -44,7 +72,17 @@ const App: () => Node = () => {
 
           if (pairingResult) {
             console.log('pairing to', RIDER_DEVICE_NAME, 'success');
+            pairingStatus = true;
             Alert.alert('Pairing Successful.');
+
+            const bleStack =
+              await driver.discoverAllServicesAndCharacteristics();
+            const allServices = await bleStack.services();
+            gpsService = allServices[0]; // Service item
+
+            // init file service
+            await gpsService.readCharacteristic(CLOSE_DIR_UUID);
+            await gpsService.readCharacteristic(OPEN_DIR_UUID);
           } else {
             console.log('sad');
             Alert.alert('Pairing Failed. Try again.');
@@ -55,107 +93,237 @@ const App: () => Node = () => {
     );
   }
 
-  async function listServices() {
-    const idk = await driver.discoverAllServicesAndCharacteristics();
-    const allServices = await idk.services();
+  let filenameB64;
+  let filenameUtf = '';
+  let filenameHex;
+  let truncatedHex;
+  let truncatedB64;
+  async function bleFileSelector() {
+    if (!pairingStatus) {
+      Alert.alert('Please connect to your StatCycle first!');
+      return;
+    }
 
-    allServices.forEach(svc => {
-      const characteristicPromise = svc.characteristics();
-      characteristicPromise.then(charArray => {
-        charArray.forEach(char => {
-          const dataPromise = char.read();
-          dataPromise.then(data => {
-            console.log('characteristic UUID:', char.uuid);
-            // console.log("id: ", data.id);
-            // console.log("serviceID: ", data.serviceID);
-            console.log(
-              'value: ',
-              Buffer.from(data.value, 'base64').toString('hex'),
-            );
-          });
-        });
-      });
-    });
+    const txtEnding = '2e747874';
+
+    const listStatus = await gpsService.readCharacteristic(LIST_DIR_UUID);
+    const listData = await listStatus.read();
+    filenameB64 = listData.value;
+    const tempName = filenameUtf;
+    filenameUtf = Buffer.from(filenameB64, 'base64').toString('utf-8');
+    if (filenameUtf.substring(0, 1) === '.') {
+      if (tempName === '') {
+        Alert.alert('There are no files stored on your Device.');
+      } else {
+        Alert.alert('There are no more files stored on your Device.');
+      }
+      return;
+    }
+
+    filenameHex = Buffer.from(filenameB64, 'base64').toString('hex');
+    const EOFindex = filenameHex.indexOf(txtEnding) + txtEnding.length;
+    truncatedHex = filenameHex.substring(0, EOFindex);
+    truncatedB64 = Buffer.from(truncatedHex, 'hex').toString('base64');
+
+    console.log('Selected File:');
+    console.log('str path:    ', filenameUtf);
+    console.log('truncatedHex:', truncatedHex);
+
+    Alert.alert('File Selected:', filenameUtf);
+  }
+
+  async function bleFileExport() {
+    if (!pairingStatus) {
+      Alert.alert('Please connect to your StatCycle first!');
+      return;
+    }
+
+    Alert.alert('File export beginning...');
+    waitforme(5000);
+
+    await gpsService.writeCharacteristicWithoutResponse(
+      OPEN_FILE_UUID,
+      truncatedB64,
+    );
+
+    //TODO: xml formatted header
+    const localFilepath = RNFS.DocumentDirectoryPath + `/${GPXfilename}`;
+    await RNFS.writeFile(localFilepath, XML_HEADER);
+
+    // for (let i = 0; i < 500; i++) {
+    for (let i = 0; i < 1000; i++) {
+      const fileCharResponse = await gpsService.readCharacteristic(
+        READ_FILE_UUID,
+      );
+
+      const fileDataPacket = await fileCharResponse.read();
+      const valB64 = fileDataPacket.value;
+      const valStr = Buffer.from(valB64, 'base64').toString('utf-8');
+      // console.log('B64 Data:', valB64);
+      if (valStr.substring(0, 4) !== '2022') {
+        break;
+      }
+      // await waitforme(300);
+      console.log('Str Data:', valStr);
+      const GPX_trkpt = formTrkpt(valStr);
+      await RNFS.appendFile(localFilepath, GPX_trkpt);
+    }
+    await RNFS.appendFile(localFilepath, '</trkseg></trk></gpx>');
+    console.log('File Export Complete');
+    Alert.alert('File Exported!');
+  }
+
+  function formTrkpt(rawString: String) {
+    let outputString = '<trkpt ';
+    const lat = rawString.substring(
+      rawString.indexOf('Latitude') + 10,
+      rawString.indexOf('Longitude') - 2,
+    );
+    const lon = rawString.substring(
+      rawString.indexOf('Longitude') + 11,
+      rawString.indexOf(', SPEED'),
+    );
+    outputString += `lat="${lat}" lon="${lon}">`;
+    const timeISO = rawString.substring(0, 19) + 'Z';
+    outputString += `<time>${timeISO}</time></trkpt>`;
+
+    return outputString;
   }
 
   async function disconnectPress() {
+    if (!pairingStatus) {
+      Alert.alert('Not currently paired to any StatCycle.');
+      return;
+    }
+
     console.log(driver.name, 'disconnected');
     driver.cancelConnection();
   }
 
-  function debugTester() {
-    console.log('debug 0');
-    console.log('debug 1');
-    console.log('debug 2');
+  function waitforme(milisec) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('');
+      }, milisec);
+    });
   }
 
-  const path = RNFS.DocumentDirectoryPath + '/statCycleGPX.txt';
-  // const gpxDir = RNFS.cacheDirectory + 'statCycleGPX/';
+  const client_id = '97923';
+  const client_secret = '1cfcd9211804b7acce56123b4624364d263e62fd';
 
-  const [content, setContent] = React.useState(null);
-  const writeFile = () => {
-    console.log(path);
-    RNFS.writeFile(path, 'This is a content from Waldo', 'utf8')
-      .then(() => console.log('FILE WRITTEN!'))
-      .catch(err => console.log(err.message));
+  const config = {
+    clientId: `${client_id}`,
+    clientSecret: `${client_secret}`,
+    redirectUrl: 'StatCycleApp://localhost',
+    serviceConfiguration: {
+      authorizationEndpoint: 'https://www.strava.com/oauth/mobile/authorize',
+      tokenEndpoint: `https://www.strava.com/oauth/token?client_id=${client_id}&client_secret=${client_secret}`,
+    },
+    scopes: ['activity:write'],
   };
-  const readFile = () => {
-    RNFS.readDir(RNFS.DocumentDirectoryPath)
-      .then(result => {
-        console.log('GOT RESULT', result);
-        return Promise.all([RNFS.stat(result[0].path), result[0].path]);
-      })
-      .then(statResult => {
-        if (statResult[0].isFile()) {
-          return RNFS.readFile(statResult[1], 'utf8');
-        }
-        return 'no file';
-      })
-      .then(contents => {
-        setContent(contents);
-        console.log(contents);
-      })
-      .catch(err => {
-        console.log(err.message, err.code);
-      });
-  };
-  const deleteFile = () => {
-    var path = RNFS.DocumentDirectoryPath + '/test.txt';
-    return RNFS.unlink(path)
-      .then(() => {
-        console.log('FILE DELETED');
-        setContent(null);
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-  };
+
+  async function POSTgpx() {
+    const file = await DocumentPicker.pick({
+      type: [DocumentPicker.types.allFiles],
+    });
+
+    console.log(file[0].uri);
+    const auth = await authorize(config);
+    const new_token = auth.accessToken;
+
+    var formdata = new FormData();
+    formdata.append('file', {
+      uri: file[0].uri,
+      name: file[0].name,
+      type: file[0].type,
+    });
+    formdata.append('name', 'StatCycle_API_Mobile0');
+    formdata.append('description', 'Formdata please');
+    formdata.append('data_type', 'gpx');
+    formdata.append('activity_type', 'Ride');
+
+    fetch('https://www.strava.com/api/v3/uploads', {
+      method: 'POST',
+      body: formdata,
+      headers: {
+        Authorization: `Bearer ${new_token}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+      .then(response => response.text())
+      .then(result => console.log(result));
+    return;
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Stat Cycle Companion</Text>
       </View>
+      <Image
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          width: 350,
+          height: 95,
+          align: 'center',
+          position: 'absolute',
+          top: 160,
+        }}
+        source={require('./UW_Eng.png')}
+      />
 
       <View style={styles.buttonContainer}>
-        {/* // TODO: byte transmission of XML GPS data */}
+        <Dialog.Container visible={visible}>
+          <Dialog.Title>Account delete</Dialog.Title>
+          <Dialog.Description>
+            Do you want to delete this account? You cannot undo this action.
+          </Dialog.Description>
+          <Dialog.Button label="Cancel" onPress={handleCancel} />
+          <Dialog.Button label="Delete" onPress={handleDelete} />
+        </Dialog.Container>
         <TouchableHighlight style={styles.button} onPress={pairStatCycle}>
           <Text style={styles.buttonText}>Scan and Connect to StatCycle</Text>
         </TouchableHighlight>
+
+        <TouchableHighlight style={styles.button} onPress={bleFileSelector}>
+          <Text style={styles.buttonText}>Select GPX File</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.button} onPress={bleFileExport}>
+          <Text style={styles.buttonText}>Export GPX File</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.button} onPress={POSTgpx}>
+          <Text style={styles.buttonText}>Post Ride to Strava</Text>
+        </TouchableHighlight>
+
         <TouchableHighlight style={styles.button} onPress={disconnectPress}>
           <Text style={styles.buttonText}>Disconnect from StatCycle</Text>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.button} onPress={listServices}>
-          <Text style={styles.buttonText}>List Services</Text>
-        </TouchableHighlight>
-        {/* // TODO: write to file system on phone */}
-        <TouchableHighlight style={styles.button} onPress={writeFile}>
-          <Text style={styles.buttonText}>XML Tester</Text>
-        </TouchableHighlight>
-        <TouchableHighlight style={styles.button} onPress={stravaInit}>
-          <Text style={styles.buttonText}>Strava Init</Text>
-        </TouchableHighlight>
       </View>
+      <Image
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          width: 200,
+          height: 150,
+          align: 'center',
+          position: 'absolute',
+          bottom: 125,
+        }}
+        source={require('./bike_clipart.png')}
+      />
+
+      <Image
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          width: 350,
+          height: 85,
+          align: 'center',
+          position: 'absolute',
+          bottom: 25,
+        }}
+        source={require('./Plexus_Logo.png')}
+      />
 
       <StatusBar style="auto" />
     </View>
@@ -172,7 +340,7 @@ const styles = StyleSheet.create({
 
   titleContainer: {
     position: 'absolute',
-    top: 70,
+    top: 100,
     align: 'center',
   },
   title: {
--- stravaHandler.js
+++ stravaHandler.js
@@ -1,8 +1,11 @@
 import {authorize} from 'react-native-app-auth';
+// import DocumentPicker from 'react-native-document-picker';
+import * as RNFS from 'react-native-fs';
 
 const client_id = '97923';
 const client_secret = '1cfcd9211804b7acce56123b4624364d263e62fd';
-const access_token = '53860b56618db38b9928d1cda59264deeccc384d';
+// const access_token = '53860b56618db38b9928d1cda59264deeccc384d';
+// const redirect_URL = 'StatCycleApp://localhost';
 
 const config = {
   clientId: `${client_id}`,
@@ -12,55 +15,155 @@ const config = {
     authorizationEndpoint: 'https://www.strava.com/oauth/mobile/authorize',
     tokenEndpoint: `https://www.strava.com/oauth/token?client_id=${client_id}&client_secret=${client_secret}`,
   },
-  scopes: ['activity:write,read'],
+  scopes: ['activity:write'],
 };
-
-export async function stravaInit() {
-  //   console.log(`id:  ${client_id}`);
-  //   console.log(`sec: ${client_secret}`);
-
-  const result = await authorize(config);
-  const res = await fetch(
-    `https://www.strava.com/oauth/deauthorize?access_token=${access_token}`,
-    {
-      method: 'POST',
-    },
-  );
-}
-// TODO: this is all for the reborn inapp browswer. OAuth2 package may be better
-// // import { Alert } from "react-native";
-// import {Alert, Linking} from 'react-native';
-// import {InAppBrowser} from 'react-native-inappbrowser-reborn';
-
-// const redirectUrl = encodeURIComponent('StatCycleApp://localhost');
-// //"http://localhost:3000/Home";
-
-// export async function stravaAuth() {
-//   const regex = /[^\\=]+[a-zA-Z0-9](?=&)/gm;
-//   const scope = 'activity:write,read';
-//   const url =
-//     'https://www.strava.com/oauth/mobile/authorize?client_id=' +
-//     '51842' + //want to be client id programatically from .env
-//     '&redirect_uri=' +
-//     redirectUrl +
-//     '&response_type=code&approval_prompt=auto&scope=' +
-//     scope;
-//   try {
-//     if (await InAppBrowser.isAvailable()) {
-//       InAppBrowser.openAuth(url, redirectUrl, {
-//         // iOS Properties
-//         ephemeralWebSession: false,
-//       }).then(response => {
-//         if (response.type === 'success' && response.url) {
-//           Linking.openURL(response.url);
-//           Alert.alert(response.url.match(regex)[0]);
-//           //   storeData(response.url.match(regex)[0]);
-//         }
-//       });
-//     } else {
-//       Linking.openURL(url);
-//     }
-//   } catch (error) {
-//     Linking.openURL(url);
-//   }
+// function uploadThing(token, path) {
+//   fetch('https://www.strava.com/api/v3/uploads', {
+//     method: 'POST',
+//     body: {
+//       file: `@file://${path}`,
+//       name: 'StatCycle_API_Mobile0',
+//       description:
+//         'this time using the iOS app (not Swagger) -- Planet_Trek file',
+//       data_type: 'gpx',
+//       activity_type: 'Ride',
+//     },
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//       // 'Content-Type': 'application/gpx+xml',
+//       'Content-Type': 'multipart/form-data',
+//     },
+//   }).then(POSTresponse => {
+//     console.log(POSTresponse);
+//   });
 // }
+
+export async function stravaAuth() {
+  // await fetch(
+  //   `https://www.strava.com/oauth/deauthorize?access_token=${access_token}`,
+  //   {
+  //     method: 'POST',
+  //   },
+  // );
+
+  const auth = await authorize(config);
+  const new_token = auth.accessToken;
+
+  const path =
+    RNFS.DocumentDirectoryPath + '/StatCycle_GPX_2022_12_12_TEST3.gpx';
+  console.log(path);
+
+  var formdata = new FormData();
+  // formdata.append('file', `@${path}`, 'StatCycle_GPX_2022_12_12_TEST3.gpx');
+  formdata.append(
+    'file',
+    RNFS.DocumentDirectoryPath,
+    '/StatCycle_GPX_2022_12_12_TEST3.gpx',
+  );
+  // formdata.append('file', `@${path}`);
+  formdata.append('name', 'StatCycle_API_Mobile0');
+  formdata.append('description', 'Formdata please');
+  formdata.append('data_type', 'gpx');
+
+  fetch('https://www.strava.com/api/v3/uploads', {
+    method: 'POST',
+    body: formdata,
+    // body: {
+    //   file: `"@${path}"`,
+    //   name: 'StatCycle_API_Mobile0',
+    //   description: 'this time using the iOS app (not Swagger) -- test file',
+    //   data_type: 'gpx',
+    //   activity_type: 'Ride',
+    // },
+    headers: {
+      Authorization: `Bearer ${new_token}`,
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+    .then(response => response.text())
+    .then(result => console.log(result));
+
+  // RNFS.readDir(RNFS.DocumentDirectoryPath)
+  //   .then(result => {
+  //     // console.log('GOT RESULT', result);
+  //     // console.log(result[0].path);
+  //     return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+  //   })
+  //   .then(statResult => {
+  //     if (statResult[0].isFile()) {
+  //       // gpxPath = statResult[0].path;
+  //       // console.log('orignal path var:', statResult[0].);
+
+  //       const myURI = 'file://' + statResult[0].path;
+  //       // const myURI =  statResult[0].path;
+  // const myName =
+  //   'Planet_Trek_Is_This_It_Flavor_InnerSpeaker_Call_Me_If_You_Get_Lost.gpx';
+  // const gpxMIME = 'application/gpx+xml';
+  // const myFile = {
+  //   uri: myURI, // e.g. 'file:///path/to/file/image123.jpg'
+  //   name: myName, // e.g. 'image123.jpg',
+  //   type: gpxMIME, // e.g. 'image/jpg'
+  // };
+
+  // const data = new FormData();
+  // data.append('file', myFile);
+  // // for (const val of data.values()) {
+  // //   console.log(val);
+  // // }
+
+  // // the @ symbol is likely necessary here in front of this path
+  // console.log('fetch path:', `@${statResult[0].path}`);
+
+  // // uploadThing(new_token, statResult[0].path);
+  // fetch('https://www.strava.com/api/v3/uploads', {
+  //   method: 'POST',
+  //   body: {
+  //     file: `@"${statResult[0].path}"`,
+  //     // file: data,
+  //     name: 'StatCycle_API_Mobile0',
+  //     description:
+  //       'this time using the iOS app (not Swagger) -- Planet_Trek file',
+  //     data_type: 'gpx',
+  //     activity_type: 'Ride',
+  //   },
+  //   headers: {
+  //     Authorization: `Bearer ${new_token}`,
+  //     'Content-Type': 'multipart/form-data',
+  //   },
+  // }).then(POSTresponse => {
+  //   console.log(POSTresponse);
+  // });
+
+  // return RNFS.readFile(statResult[1], 'utf8');
+  // return 'content here (too long)';
+  // }
+  //   return 'no file';
+  // })
+  // .then(contents => {
+  //   console.log(contents);
+  // });
+
+  // TODO: this checks the status of an upload -- likely working, just don't have the ID of an upload since none have worked
+  // const longID = 123456;
+  // const getUploadStatus = await fetch('https://www.strava.com/api/v3/uploads', {
+  //   method: 'POST',
+  //   body: {
+  //     uploadId: longID,
+  //   },
+  //   headers: {
+  //     Authorization: `Bearer ${new_token}`,
+  //   },
+  // });
+
+  // TODO: this works with scope == activity:read_all
+  //   const getActivity = await fetch(
+  //     'https://www.strava.com/api/v3/activities/8232476919',
+  //     {
+  //       method: 'GET',
+  //       headers: {
+  //         Authorization: `Bearer ${new_token}`,
+  //       },
+  //     },
+  //   );
+  //   console.log(getActivity);
+}
